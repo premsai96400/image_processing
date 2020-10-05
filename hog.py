af20731e6c69a120de7081212a65e9b532d52271
premsai96400 <56737402+premsai96400@users.noreply.github.com>
2020-10-05 11:51:38 +0530

    Update hog.py

--- hog.py
+++ hog.py
@@ -1,29 +1,14 @@
-from skimage.io import imread, imshow
-from skimage.transform import resize
-from skimage.feature import hog
-from skimage import exposure
-import matplotlib.pyplot as plt
+import requests
+import cv2
+import numpy as np
+url = 'http://192.168.43.1:8080/shot.jpg'
 
+while True:
+    imgResp = requests.get(url)
+    imgNp = np.array(bytearray(imgResp.content), dtype=np.uint8)
+    img = cv2.imdecode(imgNp, -1)
 
-
-image = imread("C:\\Users\\Hi\\Pictures\\Camera Roll\\kiran.jpg")
-image = resize(image, (400,400))
-#image = imread("E:\\asl_data set\\b\\hand1_b_bot_seg_1_cropped.jpeg")
-
-fd, hog_image = hog(image, orientations=9, pixels_per_cell=(8, 8),cells_per_block=(2, 2), visualize=True, multichannel=True)
-                    
-
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
-
-ax1.axis('off')
-ax1.imshow(image, cmap=plt.cm.gray)
-ax1.set_title('Input image')
-
-# Rescale histogram for better display
-hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 6))
-
-ax2.axis('off')
-ax2.imshow(hog_image)
-ax2.set_title('Histogram of Oriented Gradients')
-plt.show()
-print(fd,len(fd))
+    cv2.imshow("android_cam",img)
+    if cv2.waitKey(1)==27:
+        break
+cv2.destroyAllWindows()
